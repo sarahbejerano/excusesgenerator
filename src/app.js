7c07afa809c3504fb9b2e400802a6b6e6a65759b
sarahbejerano <bejeranosarah@gmail.com>
2021-10-04 16:21:41 +0000

    corractions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".es"];
+
+window.onload = function domainGenerator(pronoun, adj, noun, domain) {
+  const pronounIndex = Math.floor(Math.random() * pronoun.length);
+  const adjIndex = Math.floor(Math.random() * adj.length);
+  const nounIndex = Math.floor(Math.random() * noun.length);
+  const domainIndex = Math.floor(Math.random() * domain.length);
+  return (
+    pronoun[pronounIndex] +
+    adj[adjIndex] +
+    noun[nounIndex] +
+    domain[domainIndex]
+  );
 };
+
+function displayDomain() {
+  var finaldomain = domainGenerator();
+  document.getElementById(finaldomain).innerHTML = "";
+}
+
+console.log(displayDomain);
